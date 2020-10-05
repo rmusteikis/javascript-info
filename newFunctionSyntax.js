2873f68e7045f1321418d6cc50e1b33912e0b601
Ramūnas Musteikis <ramunasm@wix.com>
2020-10-05 15:52:15 +0300

    add 'new Function' syntax

---
+++ newFunctionSyntax.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const sum0 = new Function("a", "b", "return a + b");
+// console.log(sum0(1, 2));
+
+const sayHi = new Function("console.log('Hello')");
+// sayHi();
+
+// new Function allows to turn any string into a function.
+// For example, we can receive a new function from a server and then execute it:
+// let str = ... receive code from a server dynamically ...
+// let func = new Function(str);
+// func();
